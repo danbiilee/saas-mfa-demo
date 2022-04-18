e6fc618a9dbbda926536b03a6297a52f24551c45
danbiilee <danbi.db@gmail.com>
2022-04-18 18:34:42 +0900

    add(shared): styled-component 적용

--- shared/components/Button.tsx
+++ shared/components/Button.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Button = ({ onClick, children }: { onClick: any; children: any }) => {
-  return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: 'darkblue', color: 'white' }}
-    >
-      {children}
-    </button>
-  );
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 };
+
+const StyledButton = styled.button`
+  background-color: darkblue;
+  color: #fff;
+`;
 
 export default Button;
