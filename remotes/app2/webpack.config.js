7d631e1517ca6622a461d59a47fba5d486194fb6
danbiilee <danbi.db@gmail.com>
2022-04-18 18:24:14 +0900

    chore(remotes): 기본 환경변수 추가

--- remotes/app2/webpack.config.js
+++ remotes/app2/webpack.config.js
@@ -15,7 +15,11 @@ if (isDevelopment) {
   dotenv.config({ path: '../../.env.production' });
 }
 
-const { TE4M_MF_PORT_APP2: port, TE4M_MF_NAME_APP2: app2Name } = process.env;
+const {
+  TE4M_MF_PORT_APP2: port,
+  TE4M_MF_NAME_APP2: app2Name,
+  TE4M_MF_URL_APP2: app2URL,
+} = process.env;
 
 // Module Federation's Configuration
 const mfConfig = {
@@ -42,6 +46,10 @@ const config = merge(base(__dirname, port), {
     },
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.PUBLIC_URL': JSON.stringify(app2URL),
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
     new ModuleFederationPlugin(mfConfig),
     isDevelopment &&
       new MFLiveReloadPlugin({
